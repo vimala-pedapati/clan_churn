db6e341ed70a3cb6035d9ef4741fe08dfcbefe7f
DevPplforce <devajyoti@pplforce.com>
2024-05-15 08:55:03 +0530

    15th May

--- lib/utils/typography.dart
+++ lib/utils/typography.dart
@@ -11,13 +11,15 @@ class ClanChurnTypography {
   static TextStyle font14500 = const TextStyle(fontSize: 14, fontWeight:FontWeight.w500 ,);
   static TextStyle font14600 = const TextStyle(fontSize: 14, fontWeight:FontWeight.w600 ,);
   static TextStyle font15600 = const TextStyle(fontSize: 15, fontWeight:FontWeight.w600 ,);
-  static TextStyle font15500 = const TextStyle(fontSize: 15, fontWeight:FontWeight.w600 ,);
+  static TextStyle font15500 = const TextStyle(fontSize: 15, fontWeight:FontWeight.w500 ,);
   static TextStyle font15400 = const TextStyle(fontSize: 15, fontWeight:FontWeight.w400 ,);
+  static TextStyle font15900 = const TextStyle(fontSize: 15, fontWeight:FontWeight.w900 ,);
   static TextStyle font16700 = const TextStyle(fontSize: 16, fontWeight:FontWeight.w700 ,);
   static TextStyle font16500 = const TextStyle(fontSize: 16, fontWeight:FontWeight.w500 ,);
   static TextStyle font16600 = const TextStyle(fontSize: 16, fontWeight:FontWeight.w600 ,);
   static TextStyle font18500 = const TextStyle(fontSize: 18, fontWeight:FontWeight.w500 ,);
   static TextStyle font18600 = const TextStyle(fontSize: 18, fontWeight:FontWeight.w600 ,);
+  static TextStyle font18900 = const TextStyle(fontSize: 18, fontWeight:FontWeight.w900 ,);
   static TextStyle font22600 = const TextStyle(fontSize: 22, fontWeight:FontWeight.w600 ,);
   static TextStyle font24600 = const TextStyle(fontSize: 24, fontWeight:FontWeight.w600 ,);
   static TextStyle font48400 = const TextStyle(fontSize: 48, fontWeight:FontWeight.w400 ,);
