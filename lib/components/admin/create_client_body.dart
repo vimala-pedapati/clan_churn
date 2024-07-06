9ae9ace72398dba4d5939f29ff88815f46a36713
vimalapedapati <vimalapedapati1999@gmail.com>
2024-07-06 11:07:05 +0530

    added page view for clients and users

--- lib/components/admin/create_client_body.dart
+++ lib/components/admin/create_client_body.dart
@@ -14,6 +14,31 @@ class CreateClientBody extends StatefulWidget {
 }
 
 class _CreateClientBodyState extends State<CreateClientBody> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  void _goToNextPage() {
+    if (_currentPage < 2) {
+      _currentPage++;
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void _goToPreviousPage() {
+    if (_currentPage > 0) {
+      _currentPage--;
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -27,23 +52,77 @@ class _CreateClientBodyState extends State<CreateClientBody> {
           const SizedBox(
             height: 20,
           ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: BlocBuilder<ProjectArchitectBloc, ProjectArchitectState>(
-                builder: (context, state) {
-                  return Wrap(
-                    runSpacing: 10,
-                    spacing: 10,
-                    children: [
-                      ...state.clientList.map((c) {
-                        return AdminClientCard(
-                          client: c,
-                        );
-                      }).toList(),
-                      const CreateNewClientCard()
-                    ],
-                  );
+          Container(
+            child: Row(children: [
+              InkWell(
+                onTap: () {
+                  _goToPreviousPage();
                 },
+                child: Container(
+                    color: Theme.of(context).colorScheme.primary,
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      "Clients",
+                      style: ClanChurnTypography.font16600.copyWith(
+                          color: Theme.of(context).colorScheme.background),
+                    )),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              InkWell(
+                onTap: () {
+                  _goToNextPage();
+                },
+                child: Container(
+                    color: Theme.of(context).colorScheme.primary,
+                    padding: const EdgeInsets.all(8),
+                    child: Text(
+                      "Users",
+                      style: ClanChurnTypography.font16600.copyWith(
+                          color: Theme.of(context).colorScheme.background),
+                    )),
+              ),
+            ]),
+          ),
+          Expanded(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  border:
+                      Border.all(color: Theme.of(context).colorScheme.primary)),
+              child: PageView(
+                controller: _pageController,
+                physics: const NeverScrollableScrollPhysics(),
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                children: [
+                  SingleChildScrollView(
+                    child: BlocBuilder<ProjectArchitectBloc,
+                        ProjectArchitectState>(
+                      builder: (context, state) {
+                        return Wrap(
+                          runSpacing: 10,
+                          spacing: 10,
+                          children: [
+                            ...state.clientList.map((c) {
+                              return AdminClientCard(
+                                client: c,
+                              );
+                            }).toList(),
+                            const CreateNewClientCard()
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  const UsersCard()
+                ],
               ),
             ),
           )
@@ -84,6 +163,17 @@ class CreateNewClientCard extends StatelessWidget {
           )
         ]),
       ),
+    );
+  }
+}
+
+class UsersCard extends StatelessWidget {
+  const UsersCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("users"),
     );
   }
 }
