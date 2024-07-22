73ca93a2d7972d583b7983ed0e9512812e6e4665
vimalapedapati <vimalapedapati1999@gmail.com>
2024-07-22 18:35:13 +0530

    implementated logic for dependecy column selection and deselection

--- lib/components/input_sheet_columns.dart
+++ lib/components/input_sheet_columns.dart
@@ -159,14 +159,28 @@ class InputSheetColumns extends StatelessWidget {
                     children: [
                       Checkbox(
                         value: column.isUserCheckedIn,
-                        onChanged: column.isMandatory
+                        onChanged: column.isMandatory && !hasDependentColumns(column, columnsList)
                             ? null
                             : (value) {
-                                ColumnDetails updatedColumn =
-                                    column.copyWith(isUserCheckedIn: value);
                                 List<ColumnDetails> updatedList =
                                     columnsList.toList();
+                                ColumnDetails updatedColumn =
+                                    column.copyWith(isUserCheckedIn: value);
+
+                                // Update the current column
                                 updatedList[globalIndex] = updatedColumn;
+
+                                // Handle dependencies
+                                if (value == false) {
+                                  // Uncheck dependent columns if unchecked
+                                  updatedList = handleDependencies(
+                                      updatedList, updatedColumn.id, false);
+                                } else {
+                                  // Check dependent columns if checked
+                                  updatedList = handleDependencies(
+                                      updatedList, updatedColumn.id, true);
+                                }
+
                                 context.read<ProjectArchitectBloc>().add(
                                     ReplaceColumnsEvent(
                                         columns: updatedList,
@@ -192,36 +206,7 @@ class InputSheetColumns extends StatelessWidget {
                                 enabled: column.isUserCheckedIn,
                                 cursorHeight: 15,
                                 onChanged: (value) {
-                                  // Update the text of the controller
-                                  // customerColumnNames[globalIndex].text = value;
-
-                                  // Get the current cursor position
-                                  // int cursorPosition = value.length;
-
-                                  // Update the selection of the controller
-                                  // customerColumnNames[globalIndex].selection =
-                                  //     TextSelection.collapsed(
-                                  //         offset: cursorPosition);
-
-                                  // Dispatch an event to update the state with the modified list of controllers
-                                  customerColumnNames.replaceRange(
-                                      globalIndex,
-                                      globalIndex + 1,
-                                      [TextEditingController(text: value)]);
-                                  for (var i in customerColumnNames) {
-                                    print(i);
-                                  }
-
-                                  // context.read<ProjectArchitectBloc>().add(
-                                  //       CustomerColumnNamesEvent(
-                                  //           customerColumnNames:
-                                  //               customerColumnNames),
-                                  //     );
-                                },
-                                onEditingComplete: () {
-                                  for (var i in customerColumnNames) {
-                                    print(i);
-                                  }
+                                  customerColumnNames[globalIndex].text = value;
                                 },
                                 decoration: InputDecoration(
                                     hintText: "$globalIndex ${column.id}",
@@ -244,6 +229,12 @@ class InputSheetColumns extends StatelessWidget {
     );
   }
 
+  bool hasDependentColumns(ColumnDetails column, List<ColumnDetails> columns) {
+    List<ColumnDetails> dependentColumns =
+        columns.where((col) => col.depends == column.id).toList();
+    return dependentColumns.isNotEmpty;
+  }
+
   // Function to group ColumnDetails by sheetName
   Map<String, List<ColumnDetails>> groupBySheetName(
       List<ColumnDetails> columns) {
@@ -256,7 +247,167 @@ class InputSheetColumns extends StatelessWidget {
     }
     return groupedColumns;
   }
+
+  List<ColumnDetails> handleDependencies(
+      List<ColumnDetails> columnsList, String columnId, bool isChecked) {
+    List<ColumnDetails> updatedList = columnsList.toList();
+
+    for (int i = 0; i < updatedList.length; i++) {
+      if (updatedList[i].depends == columnId) {
+        updatedList[i] = updatedList[i].copyWith(isUserCheckedIn: isChecked);
+        if (!isChecked) {
+          // If unchecking, recursively uncheck dependents
+          updatedList =
+              handleDependencies(updatedList, updatedList[i].id, false);
+        }
+      }
+    }
+
+    return updatedList;
+  }
 }
+
+// class InputSheetColumns extends StatelessWidget {
+//   final List<ColumnDetails> columnsList;
+//   final List<TextEditingController> customerColumnNames;
+
+//   const InputSheetColumns({
+//     Key? key,
+//     required this.columnsList,
+//     required this.customerColumnNames,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Grouping the columns by sheetName
+//     Map<String, List<ColumnDetails>> groupedColumns =
+//         groupBySheetName(columnsList);
+
+//     return SingleChildScrollView(
+//       child: Column(
+//         children: groupedColumns.entries.map((entry) {
+//           String sheetName = entry.key;
+//           List<ColumnDetails> columns = entry.value;
+
+//           return Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Padding(
+//                 padding: const EdgeInsets.symmetric(vertical: 8.0),
+//                 child: Text(
+//                   "\n $sheetName",
+//                   style: const TextStyle(
+//                       fontSize: 18, fontWeight: FontWeight.bold),
+//                 ),
+//               ),
+//               Wrap(
+//                 spacing: 0,
+//                 runSpacing: 0,
+//                 children: List.generate(columns.length, (index) {
+//                   ColumnDetails column = columns[index];
+//                   int globalIndex = columnsList.indexOf(column);
+//                   return Row(
+//                     children: [
+//                       Checkbox(
+//                         value: column.isUserCheckedIn,
+//                         onChanged: column.isMandatory
+//                             ? null
+//                             : (value) {
+//                                 ColumnDetails updatedColumn =
+//                                     column.copyWith(isUserCheckedIn: value);
+//                                 List<ColumnDetails> updatedList =
+//                                     columnsList.toList();
+//                                 updatedList[globalIndex] = updatedColumn;
+//                                 context.read<ProjectArchitectBloc>().add(
+//                                     ReplaceColumnsEvent(
+//                                         columns: updatedList,
+//                                         index: globalIndex));
+//                               },
+//                       ),
+//                       Expanded(
+//                         child: Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                           crossAxisAlignment: CrossAxisAlignment.center,
+//                           children: [
+//                             Expanded(
+//                               child: Text(
+//                                 column.columnName,
+//                                 overflow: TextOverflow.visible,
+//                               ),
+//                             ),
+//                             SizedBox(
+//                               height: 30,
+//                               width: 300,
+//                               child: TextFormField(
+//                                 controller: customerColumnNames[globalIndex],
+//                                 enabled: column.isUserCheckedIn,
+//                                 cursorHeight: 15,
+//                                 onChanged: (value) {
+//                                   // Update the text of the controller
+//                                   // customerColumnNames[globalIndex].text = value;
+
+//                                   // Get the current cursor position
+//                                   // int cursorPosition = value.length;
+
+//                                   // Update the selection of the controller
+//                                   // customerColumnNames[globalIndex].selection =
+//                                   //     TextSelection.collapsed(
+//                                   //         offset: cursorPosition);
+
+//                                   // Dispatch an event to update the state with the modified list of controllers
+//                                   customerColumnNames.replaceRange(
+//                                       globalIndex,
+//                                       globalIndex + 1,
+//                                       [TextEditingController(text: value)]);
+//                                   for (var i in customerColumnNames) {
+//                                     print(i);
+//                                   }
+
+//                                   // context.read<ProjectArchitectBloc>().add(
+//                                   //       CustomerColumnNamesEvent(
+//                                   //           customerColumnNames:
+//                                   //               customerColumnNames),
+//                                   //     );
+//                                 },
+//                                 onEditingComplete: () {
+//                                   for (var i in customerColumnNames) {
+//                                     print(i);
+//                                   }
+//                                 },
+//                                 decoration: InputDecoration(
+//                                     hintText: "$globalIndex ${column.id}",
+//                                     contentPadding: const EdgeInsets.only(
+//                                         left: 10, right: 10),
+//                                     border: const OutlineInputBorder()),
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                     ],
+//                   );
+//                 }),
+//               ),
+//             ],
+//           );
+//         }).toList(),
+//       ),
+//     );
+//   }
+
+//   // Function to group ColumnDetails by sheetName
+//   Map<String, List<ColumnDetails>> groupBySheetName(
+//       List<ColumnDetails> columns) {
+//     Map<String, List<ColumnDetails>> groupedColumns = {};
+//     for (var column in columns) {
+//       if (!groupedColumns.containsKey(column.sheetName)) {
+//         groupedColumns[column.sheetName] = [];
+//       }
+//       groupedColumns[column.sheetName]?.add(column);
+//     }
+//     return groupedColumns;
+//   }
+// }
 // import 'dart:developer';
 // // ignore: avoid_web_libraries_in_flutter
 // import 'dart:html';
