6feffe4d0df4a0748766afe993c0e49bc6009ed7
vimalapedapati <vimalapedapati1999@gmail.com>
2024-07-22 17:21:27 +0530

    added new parameters for column model

--- lib/api_repos/models/column_model.dart
+++ lib/api_repos/models/column_model.dart
@@ -16,6 +16,9 @@ class ColumnDetails extends Equatable {
   final bool isMandatory;
   final bool isUserCheckedIn;
   final String clientColumnName;
+  final bool? isEditable;
+  final String? depends;
+  final String? columnDataType;
 
   const ColumnDetails(
       {required this.id,
@@ -23,7 +26,10 @@ class ColumnDetails extends Equatable {
       required this.columnName,
       required this.isMandatory,
       required this.isUserCheckedIn,
-      required this.clientColumnName});
+      required this.clientColumnName,
+      required this.isEditable,
+      required this.depends,
+      required this.columnDataType});
 
   factory ColumnDetails.fromJson(Map<String, dynamic> json) => ColumnDetails(
       id: json["id"],
@@ -31,15 +37,20 @@ class ColumnDetails extends Equatable {
       columnName: json["column_name"],
       isMandatory: json["is_mandatory"],
       isUserCheckedIn: json["is_mandatory"] ?? false,
-      clientColumnName:
-          json["client_column_name"] ?? json["column_name"] ?? "");
+      clientColumnName: json["client_column_name"] ?? json["column_name"] ?? "",
+      isEditable: json["is_editable"],
+      depends: json["depends"],
+      columnDataType: json["column_data_type"]);
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "sheet_name": sheetName,
         "column_name": columnName,
         "is_mandatory": isMandatory,
-        "client_column_name": clientColumnName
+        "client_column_name": clientColumnName,
+        "is_editable": isEditable,
+        "depends": depends,
+        "column_data_type": columnDataType
       };
 
   ColumnDetails copyWith(
@@ -48,20 +59,29 @@ class ColumnDetails extends Equatable {
       String? columnName,
       bool? isMandatory,
       bool? isUserCheckedIn,
-      String? clientColumnName}) {
+      String? clientColumnName,
+      bool? isEditable,
+      String? depends,
+      String? columnDataType}) {
     return ColumnDetails(
         id: id ?? this.id,
         sheetName: sheetName ?? this.sheetName,
         columnName: columnName ?? this.columnName,
         isMandatory: isMandatory ?? this.isMandatory,
         isUserCheckedIn: isUserCheckedIn ?? this.isUserCheckedIn,
-        clientColumnName: clientColumnName ?? this.clientColumnName);
+        clientColumnName: clientColumnName ?? this.clientColumnName,
+        isEditable: isEditable ?? this.isEditable,
+        depends: depends ?? this.depends,
+        columnDataType: columnDataType ?? this.columnDataType);
   }
 
   @override
   List<Object?> get props => [
         columnName,
         isUserCheckedIn,
-        clientColumnName
+        clientColumnName,
+        isEditable,
+        depends,
+        columnDataType
       ];
 }
